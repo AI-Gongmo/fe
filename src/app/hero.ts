b57fa359842ff393d71d4dccff63a27d3ef8fc32
YeChan Kim <az20058@naver.com>
2025-10-12 03:58:02 +0900

    Settings: heroui 추가

---
+++ src/app/hero.ts
@@ -0,0 +1,19 @@
+import { heroui } from '@heroui/react';
+export default heroui({
+  themes: {
+    light: {
+      colors: {
+        primary: {
+          DEFAULT: '#01A729',
+        },
+      },
+    },
+    dark: {
+      colors: {
+        primary: {
+          DEFAULT: '#01A729',
+        },
+      },
+    },
+  },
+});
